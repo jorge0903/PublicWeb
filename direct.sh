21fb1ac55154e160fa8b3e3ce4d3b6d39061f984
jorge0903 <46610536+jorge0903@users.noreply.github.com>
2019-08-05 06:32:28 -0500

    Update direct.sh

--- direct.sh
+++ direct.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd .ShameOnMe
+cd Pictures/
 NAME=$((10000 + RANDOM % 100000000))
 echo $NAME
 
-#curl -k $1 --output $NAME.jpg
-#curl -k $1 --output $NAME.png
 curl -k $1 > $NAME.png
 echo -e "\n$NAME.png\n"
 cd ..
